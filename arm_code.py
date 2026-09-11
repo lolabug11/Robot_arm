03594375b7331225ae7f30dbf50af87fdee460ae
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2026-09-11 22:57:12 +0000

    fixed some issues with copy and pasting and some spelling errors

--- arm_code.py
+++ arm_code.py
@@ -9,68 +9,59 @@ while True:
     ring_speed_mode = 3
     pinky_speed_mode = 3
     selected = None
-    
     while select:
         if "1".is_pressed():
             selected = 1
         elif "2".is_pressed():
             selected = 2
         elif "3".is_pressed():
-            select = 3
+            selected = 3
         elif "4".is_pressed():
-            select = 4
+            selected = 4
         elif "5".is_pressed():
-            select = 5
+            selected = 5
         while selected == 1:
             if "ch+".is_clicked():
                 if thumb_speed_mode < 5:
                     thumb_speed_mode += 1
-                    ring_speed_mode += 1
                     selected = -1
                     select = False
             elif "ch-".is_clicked():
                 if thumb_speed_mode > 1:
                     thumb_speed_mode -= 1
-                    ring_speed_mode += 1
                     selected = -1
                     select = False
         while selected == 2:
             if "ch+".is_clicked():
                 if pointer_speed_mode < 5:
                     thumb_speed_mode += 1
-                    ring_speed_mode += 1
                     selected = -1
                     select = False
             elif "ch-".is_clicked():
                 if pointer_speed_mode > 1:
                     pointer_speed_mode -= 1
-                    ring_speed_mode += 1
                     selected = -1
                     select = False  
         while selected == 3:
             if "ch+".is_clicked():
                 if pointer_speed_mode < 5:
                     pointer_speed_mode += 1
-                    ring_speed_mode += 1
                     selected = -1
                     select = False
             elif "ch-".is_clicked():
                 if pointer_speed_mode > 1:
                     pointer_speed_mode -= 1  
-                    ring_speed_mode += 1
                     selected = -1
                     select = False
         while selected == 4:
             if "ch+".is_clicked():
                 if middle_speed_mode < 5:
                     middle_speed_mode += 1
-                    ring_speed_mode += 1
                     selected = -1
                     select = False
             elif "ch-".is_clicked():
                 if middle_speed_mode > 1:
                     middle_speed_mode -= 1
-                    ring_speed_mode += 1
                     selected = -1
                     select = False
         while selected == 5:
@@ -82,6 +73,5 @@ while True:
             elif "ch-".is_clicked():
                 if ring_speed_mode > 1:
                     ring_speed_mode -= 1
-                    ring_speed_mode += 1
                     selected = -1
                     select = False
